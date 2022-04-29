e2a68a3560898e8add0b7b11e3ffe322e2fc650f
Adroaldo <hygino@msn.com>
2022-04-29 20:34:03 -0300

    Criando o endpoint pra salvar no banco de dados

--- src/main/java/dev/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ src/main/java/dev/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -8,40 +8,30 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.com.springboot.curso_jdev_treinamento.model.Usuario;
 import dev.com.springboot.curso_jdev_treinamento.service.UsuarioService;
 
 @RestController
-@RequestMapping("/usuario")
+@RequestMapping(value = "/usuario")
 public class GreetingsController {
 	@Autowired
 	private UsuarioService usuarioService;
 
-	@PostMapping
-	public Usuario salvarUsuario(@RequestBody Usuario usuario) {
-		return usuarioService.salvarUsuario(usuario);
+	@PostMapping(value = "salvar")
+	@ResponseBody
+	public ResponseEntity<Usuario> salvarUsuario(@RequestBody Usuario usuario) {
+		Usuario user = usuarioService.salvarUsuario(usuario);
+		
+		return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
 	}
 
-	@RequestMapping(value = "mostrarnome/{name}", method = RequestMethod.GET)
-	@ResponseStatus(HttpStatus.OK)
-	public String greetingText(@PathVariable String name) {
-		return "Curso Spring Boot API: " + name + "!";
-	}
-
-	@GetMapping("/olamundo/{nome}")
-	@ResponseStatus(HttpStatus.OK)
-	public String metodoRetornaOlaMundo(@PathVariable String nome) {
-		return "Olá mundo : " + nome;
-	}
-
-	@GetMapping("listatodos")
+	@GetMapping(value = "listatodos")
 	@ResponseBody // retorna o corpo da resposta
 	public ResponseEntity<List<Usuario>> listaUsuario() {
 		List<Usuario> usuarios = usuarioService.listaUsuario();
@@ -49,11 +39,16 @@ public class GreetingsController {
 		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
 	}
 
-	@GetMapping("{id}")
+	@GetMapping(value = "{id}")
 	@ResponseBody
 	public ResponseEntity<Usuario> buscarUsuarioPorId(@PathVariable long id) {
 		Usuario usuario = usuarioService.buscarUsuarioPorId(id);
 
 		return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
 	}
+	
+//	@PutMapping(value = "{id}")
+//	public ResponseEntity<Usuario> atualizarUsuarioPorId(@PathVariable long id,@RequestBody Usuario usuario){
+//		
+//	}
 }
